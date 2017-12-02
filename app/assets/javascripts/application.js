fa6cfe8c88e8695779160d8e0106b71ba65cd73c
Peter <pagrillet@gmail.com>
2017-12-02 09:38:33 +0000

    Added ajax rendering of results of the form searching foor stocks. Still an issue with jquery not working properly

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+  $('#stock-lookup-form').on('ajax:complete', function(event, data, status){
+    $('#results').html(data.responseText)
+  })
+})
--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,9 +1,21 @@
 class StocksController < ApplicationController
 	
-	def search
-		@stock = Stock.new_from_lookup(params[:stock])
-		render 'users/my_portfolio'
-	end
+def search
+  if params[:stock].present?
+    @stock = Stock.new_from_lookup(params[:stock])
+    if @stock
+      respond_to do |format|
+        format.js { render partial: 'users/result' }
+      end
+    else
+      flash[:danger] = "You have entered an incorrect symbol"
+      redirect_to my_portfolio_path
+    end
+  else
+    flash[:danger] = "You have entered an empty search string"
+    redirect_to my_portfolio_path
+  end
+end
 
 	
 
