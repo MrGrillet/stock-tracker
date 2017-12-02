c7fb44b07d8687f39090bd4424acc2b25592422d
Peter <pagrillet@gmail.com>
2017-12-02 12:33:24 +0000

    Added associations so stocks can be linked to users. Also added the ability to be able to delete the realtionships.

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -9,6 +9,13 @@ class UserStocksController < ApplicationController
 		@user_stock = UserStock.create(user: current_user, stock: stock)
 		flash[:success] = "#{@user_stock.stock.name} was added to your portfolio"
 		redirect_to my_portfolio_path
-		
+	end
+
+	def destroy
+		stock = Stock.find(params[:id])
+		@user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+		@user_stock.destroy
+		flash[:notice] = "Stock deleted"
+		redirect_to my_portfolio_path
 	end
 end
